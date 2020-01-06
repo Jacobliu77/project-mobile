d7c71ef8cef56f98d7702c3dcb92c3c0cb4c9b4c
e <751232478@qq.com>
2020-01-06 11:35:34 +0800

    项目初始化--引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <!-- 一级路由视图区 -->
-    <router-view/>
+    <van-cell title="选择单个日期" :value="date" @click="show = true" />
+    <van-calendar v-model="show" @confirm="onConfirm" />
+    <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      date: '',
+      show: false
+    }
+  },
+
+  methods: {
+
+  }
 }
 </script>
 <style lang="less" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/utils/register-vant.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import { Button, Cell, CellGroup, Calendar } from 'vant'
+
+Vue.use(Button)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Calendar)
