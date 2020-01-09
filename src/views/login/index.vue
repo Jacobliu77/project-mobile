e877f28ec84add2ee9434421995b6a1d241731cf
e <751232478@qq.com>
2020-01-09 11:36:03 +0800

    对指定的数据字段进行校验（在发送验证码后对手机号的校验）

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,8 +36,8 @@
   v-if="isCountDownShow"
   slot="button"
   format=" ss 秒后可重新发送"
-  :time="1000*60"
-  @finish="this.isCountDownShow=false" />
+  :time="1000*10"
+  @finish="isCountDownShow = false" /> <!-- 在倒计时结束后触发隐藏 -->
    <van-button
     v-else
     slot="button"
@@ -57,6 +57,7 @@
 <script>
 // 引入请求模块
 import { login, smscode } from '@/api/user'
+import { validate } from 'vee-validate'
 
 export default {
   name: 'LoginPage',
@@ -73,7 +74,21 @@ export default {
     async onLogin () {
       const success = await this.$refs.myloginform.validate()
       if (!success) {
-        console.log('验证失败')
+        // 这里加定时器的原因是因为获取验证失败的结果有延迟问题，并不是我们的原因
+        setTimeout(() => {
+          const errors = this.$refs.myloginform.errors
+          // forEach 无法停止
+          // find 方法会遍历数组，对每个元素执行方法中的条件判定
+          // const item = Object.values(errors).find(item => item[0])
+          // 找到第1个有错误的字段并弹出提示
+          for (let key in errors) {
+            const item = errors[key]
+            if (item[0]) {
+              this.$toast(item[0])
+              return
+            }
+          }
+        }, 100)
         return
       }
       this.$toast.loading({
@@ -86,12 +101,19 @@ export default {
         console.log('登录成功', res)
         this.$toast.success('登录成功')
       } catch (err) {
-        console.log('登录失败', err)
         this.$toast.fail('登录失败，手机号或验证码错误')
       }
     },
     async onsmscode () {
       const { mobile } = this.user
+      const validateResult = await validate(mobile, 'required|mobile', {
+        name: '手机号'
+      })
+      // 如果验证失败，提示错误消息，停止发送验证码
+      if (!validateResult.valid) {
+        this.$toast(validateResult.errors[0])
+        return
+      }
       try {
         // 显示倒计时
         this.isCountDownShow = true
